a052bf14bb3442a23dd661429eb5929498d056ac
Vitoralenkart <vitoralencarbs@gmail.com>
2026-05-14 16:00:17 -0300

    Fix some parts of project

--- DTOs/StudentDTO.cs
+++ DTOs/StudentDTO.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ProjectManagementAPI.DTOs
 {
@@ -10,6 +11,7 @@ namespace ProjectManagementAPI.DTOs
         public required string FullName { get; set; }
         public required string Email { get; set; }      
         public string EducationalInstitution { get; set; }
+        [Required]
         public string Role { get; set; }
     }
 }
